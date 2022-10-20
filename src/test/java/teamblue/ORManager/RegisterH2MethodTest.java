4e326f8c3d245351fb34d550f36aa25b8694e3a2
Tomasz Borowski <bortom112233@gmail.com>
2022-10-20 16:03:26 +0200

    added test for proper functioning of register(Class... classes) method

---
+++ src/test/java/teamblue/ORManager/RegisterH2MethodTest.java
@@ -0,0 +1,32 @@
+package teamblue.ORManager;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RegisterH2MethodTest {
+
+    @Test
+    public void whenRegisterMethodInH2ORManagerInvokedWithBookClass_shouldAddTableWithGivenColumns() throws SQLException {
+
+        ORManager orManager = ORManagerFactory.withPropertiesFrom("src/test/resources/db.file");
+        orManager.register(BookTest.class);
+
+        PreparedStatement preparedStatement = orManager.getConnectionWithDB().prepareStatement("SELECT * FROM BOOKS");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        int IDColumnNr = resultSet.findColumn("ID");
+        int titleColumnNr = resultSet.findColumn("Title_Of_Book");
+        int publishedAtColumnNr = resultSet.findColumn("Published_At");
+
+        Assertions.assertThat(IDColumnNr).isEqualTo(1);
+        Assertions.assertThat(titleColumnNr).isEqualTo(2);
+        Assertions.assertThat(publishedAtColumnNr).isEqualTo(3);
+
+    }
+}
