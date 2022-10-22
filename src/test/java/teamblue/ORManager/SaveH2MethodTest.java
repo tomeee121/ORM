a897916a6044a96ae0fe1b44a4f99a6def883b30
DamianGrudzien <grudzien1694@gmail.com>
2022-10-22 14:57:12 +0200

    Fix/saving id through save method (#33)
    * method save fixed, more test scenario added

--- src/test/java/teamblue/ORManager/SaveH2MethodTest.java
+++ src/test/java/teamblue/ORManager/SaveH2MethodTest.java
@@ -5,15 +5,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import teamblue.ORManager.modelTest.IdInSecondPosition;
+import teamblue.ORManager.modelTest.MissingIdAnnotation;
+import teamblue.ORManager.modelTest.WithOneIdField;
+import teamblue.annotations.Entity;
+import teamblue.annotations.Id;
+import teamblue.annotations.Table;
 import teamblue.model.Book;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class SaveH2MethodTest {
@@ -24,49 +30,124 @@ public class SaveH2MethodTest {
     @Before
     public void setUp() throws Exception {
         orManager = ORManagerFactory.withPropertiesFrom("src/test/resources/db.file");
-        try {
-            orManager.register(Book.class);
-        } catch (SQLException e) {
-            log.info("Error in SQL: {}",e.getMessage());
-        }
+        orManager.getConnectionWithDB()
+                 .prepareStatement("DROP TABLE IF EXISTS BOOKS")
+                 .execute();
     }
 
     @Test
-    public void shouldSaveSingleBookToDB_whereIdIsGeneratedAndNotNull() {
+    public void shouldSaveSingleBookToDB_whereIdIsGeneratedAndNotNull() throws SQLException {
 
         Book book = new Book("Test", LocalDate.now());
+
+        orManager.register(book.getClass());
         orManager.save(book);
+
         assertThat(book.getId()).isNotNull();
     }
 
     @Test
-    public void shouldSaveSingleBook_whereIdIsNotGenerated(){
+    public void shouldSaveSingleBook_whereIdIsPresentInObject() throws SQLException {
         Book book = new Book("Other", LocalDate.now());
+        String tableName = getTableName(book.getClass());
+
         book.setId(2L);
-        long lastId = 0L;
+        orManager.register(book.getClass());
+
+        long newId = getSizeOfTable(tableName) + 1L;
+        orManager.save(book);
+
+        assertThat(book).hasFieldOrPropertyWithValue("Id", newId);
+    }
+
+    @Test
+    public void shouldSaveObject_whenFieldsAreNull() throws SQLException {
+        // Given
+        Book book = new Book(null, null);
+        Book book1 = new Book("title", null);
+        Book book2 = new Book(null, LocalDate.of(2022, 10, 2));
+        // When
+        orManager.register(book.getClass());
+        orManager.save(book);
+        orManager.save(book1);
+        orManager.save(book2);
+        // Then
+        assertThat(book.getId()).isPositive();
+        assertThat(book1.getId()).isPositive();
+        assertThat(book2.getId()).isPositive();
+    }
+
+
+    @Test
+    public void shouldNotSaveObject_whenObjectHaveNoFields() {
+        @Entity
+        class WithoutFields {}
+
+        WithoutFields test = new WithoutFields();
+        String tableName =  getTableName(test.getClass());
         try {
-            ResultSet rs = orManager.getConnectionWithDB()
-                                    .prepareStatement("SELECT COUNT(*) FROM BOOKS")
-                                    .executeQuery();
-            while (rs.next()) {
-                lastId = rs.getLong(1);
-            }
-            orManager.save(book);
-            assertThat(book).hasFieldOrPropertyWithValue("Id",lastId + 1L);
+            orManager.register(test.getClass());
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            log.debug("{}",e.getSQLState());
         }
+
+        long sizeOfTableBeforeSave = getSizeOfTable(tableName);
+        orManager.save(test);
+        long sizeOfTableAfterSave = getSizeOfTable(tableName);
+
+        assertThat(sizeOfTableBeforeSave).isEqualTo(sizeOfTableAfterSave);
+    }
+
+    @Test
+    public void shouldSaveObject_whenObjectHaveOneIdField() throws SQLException {
+
+        WithOneIdField test = new WithOneIdField();
+        String tableName =  getTableName(test.getClass());
+        orManager.register(test.getClass());
+
+        long sizeOfTableBeforeSave = getSizeOfTable(tableName);
+        orManager.save(test);
+        long sizeOfTableAfterSave = getSizeOfTable(tableName);
+
+        assertThat(sizeOfTableBeforeSave).isNotEqualTo(sizeOfTableAfterSave);
+    }
+
+    @Test
+    public void shouldSaveObject_whenIdInObjectIsInSecondPosition() throws SQLException {
+
+        IdInSecondPosition test = new IdInSecondPosition();
+        String tableName =  getTableName(test.getClass());
+        orManager.register(test.getClass());
+
+        long sizeOfTableBeforeSave = getSizeOfTable(tableName);
+        orManager.save(test);
+        long sizeOfTableAfterSave = getSizeOfTable(tableName);
+
+        assertThat(sizeOfTableBeforeSave).isNotEqualTo(sizeOfTableAfterSave);
+    }
+
+    @Test
+    public void shouldSaveObject_whenIdIsMissing() throws SQLException {
+        MissingIdAnnotation test = new MissingIdAnnotation();
+        String tableName =  getTableName(test.getClass());
+        orManager.register(test.getClass());
+
+        long sizeOfTableBeforeSave = getSizeOfTable(tableName);
+        orManager.save(test);
+        long sizeOfTableAfterSave = getSizeOfTable(tableName);
+
+        assertThat(sizeOfTableBeforeSave).isNotEqualTo(sizeOfTableAfterSave);
     }
 
     @After
-    public void afterTest(){
+    public void afterTest() {
         Connection conn = null;
         try {
             conn = orManager.getConnectionWithDB();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        if (conn!=null){
+        if (conn != null) {
             try {
                 conn.close();
             } catch (SQLException e) {
@@ -75,24 +156,26 @@ public class SaveH2MethodTest {
         }
     }
 
+    private long getSizeOfTable(String nameOfTable) {
+        long size = 0L;
+        try {
+            ResultSet rs = orManager.getConnectionWithDB()
+                                    .prepareStatement("SELECT COUNT(*) FROM " + nameOfTable)
+                                    .executeQuery();
+            while (rs.next()) {
+                size = rs.getLong(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return size;
+    }
 
-
-
-
-//    @Test
-//    public void shouldThrowException_whereClassWasNotFound(){
-//        // Given
-//        class  BookTest{
-//        }
-//        BookTest bookTest = new BookTest();
-//        // When
-//        //Then
-//        assertThatThrownBy(() -> orManager.save(bookTest))
-//                .isInstanceOf(RuntimeException.class)
-//                .hasMessage("Class was not found");
-//    }
-
-
-
-
+    private String getTableName(Class<?> clazz){
+        if(clazz.isAnnotationPresent(Table.class)){
+            return clazz.getAnnotation(Table.class).value();
+        } else {
+            return clazz.getSimpleName();
+        }
+    }
 }
