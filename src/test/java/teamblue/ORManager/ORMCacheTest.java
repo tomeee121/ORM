893eb986fe9b5bc61e1e1f6889d10cfa015106bd
Tomasz Borowski <bortom112233@gmail.com>
2022-10-23 19:19:38 +0200

    introduced clearCache() method to prepare genuine testing environment

--- src/test/java/teamblue/ORManager/ORMCacheTest.java
+++ src/test/java/teamblue/ORManager/ORMCacheTest.java
@@ -2,6 +2,7 @@ package teamblue.ORManager;
 
 import org.assertj.core.api.Assertions;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,28 +21,29 @@ import static teamblue.constants.h2.ConstantsH2.DROP_IF_EXISTS_BOOKS;
 
 public class ORMCacheTest {
 
-
-    private static final Logger log = LoggerFactory.getLogger(ORMCacheTest.class);
     private H2ORManager orManager;
+
+    @Before
+    public void setUp() throws SQLException {
+        orManager = (H2ORManager) ORManagerFactory.withPropertiesFrom("src/test/resources/db.file");
+        H2ORManager.MetaInfo.clearCache();
+    }
 
     @Test
     public void shouldAddToCacheRightNrOfElements_whenSaveAndFindInvoked() throws SQLException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
 
-        //given
-        orManager = (H2ORManager) ORManagerFactory.withPropertiesFrom("src/test/resources/db.file");
         Assertions
                 .assertThatThrownBy(() -> H2ORManager.MetaInfo.getCache().get(Book.class).size())
-                .isInstanceOf(NullPointerException.class)
-                .hasMessage("Cannot invoke \"java.util.Set.size()\" because the return value of \"java.util.Map.get(Object)\" is null");
+                .isInstanceOf(NullPointerException.class);
 
         //when
+        H2ORManager.MetaInfo.clearCache();
         orManager.register(Book.class);
         Book harry = new Book("Harry Potter", LocalDate.of(2011, 11, 28));
         Book cypher_fortress = new Book("Cypher Fortress", LocalDate.of(1998, 11, 9));
-        List<Book> books = Arrays.asList(harry, cypher_fortress);
         orManager.save(harry);
         orManager.save(cypher_fortress);
-        orManager.findAll(Book.class);
+        List<Book> books = orManager.findAll(Book.class);
 
         //then
         int nrOfCachedElementsAfterSavingToDB = H2ORManager.MetaInfo.getCache().get(Book.class).size();
