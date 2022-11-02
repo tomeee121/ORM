581e659b2cd0b31afc645fed2d0722c5207d7eb3
Tomasz Borowski <bortom112233@gmail.com>
2022-11-02 21:14:52 +0100

    refactored demo - project presentation in main class

--- src/main/java/teamblue/ORManager/App.java
+++ src/main/java/teamblue/ORManager/App.java
@@ -8,6 +8,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 public class App {
@@ -24,17 +25,15 @@ public class App {
         orManager.register(Book.class);
 
         Book harry = new Book("Harry", LocalDate.now());
-        Book Potter = new Book("Potter", LocalDate.now());
         harry.setPublishedAt(LocalDate.of(2020, 12, 12));
-        orManager.save(harry);
-        orManager.findById(1, Book.class);
-        harry.setTitle("");
-        harry.setPublishedAt(LocalDate.of(1111, 11, 11));
-        System.out.println(harry);
-        orManager.refresh(harry);
-        System.out.println(harry);
-        harry.setPublishedAt(LocalDate.of(1900, 02, 01));
-        orManager.merge(harry);
+        Book saved = orManager.save(harry);
+        Optional<Book> byId = orManager.findById(saved.getId(), Book.class);
+        Book bookFound = byId.get();
+        bookFound.setTitle("");
+        bookFound.setPublishedAt(LocalDate.of(1111, 11, 11));
+        orManager.merge(bookFound);
+        System.out.println(orManager.findById(bookFound.getId(), Book.class).get());
+
     }
 
     private static void _1MPresentation(ORManager orManager) throws SQLException {
@@ -50,28 +49,12 @@ public class App {
         orManager.save(publisher1M);
         orManager.save(bookM1);
         orManager.save(bookM1v2);
+
+
+        Publisher publisher1Mv2 = new Publisher("IT libr");
+        orManager.persist(publisher1Mv2);
+        bookM1v2.setPublisher(publisher1Mv2);
+        orManager.merge(bookM1v2);
     }
-
-        public static void findAllCheck (ORManager orManager) throws
-        SQLException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
-            List<Book> all = orManager.findAll(Book.class);
-            System.out.println("Found elements number: " + all.size());
-            Book harry = new Book("Potter", LocalDate.now());
-            orManager.save(harry);
-            Book jj = new Book("jj", LocalDate.now());
-            orManager.save(jj);
-            List<Book> all2 = orManager.findAll(Book.class);
-            List<Book> all3 = orManager.findAll(Book.class);
-            List<Book> all4 = orManager.findAll(Book.class);
-            List<Book> all5 = orManager.findAll(Book.class);
-
-            System.out.println(all2.size() + " łącznie jest w bazie  elementów ");
-//        orManager.findAll(Book.class).forEach(System.out::println);
-
-            System.out.println(orManager.findAll(Book.class) + "ffffff");
-            System.out.println(orManager.findAll(Book.class).get(0) + "ffffff");
-
-
-        }
 
     }
